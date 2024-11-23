9b9e73fd6beb44780ef99041ce2f0e1bf8eadbfd
Khan-Lubna <116883265+Khan-Lubna@users.noreply.github.com>
2024-11-23 16:34:15 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,71 @@
+let shopItemsData = [{
+    id:"abc",
+    name:"Smart Yoga Mat",
+    price: 100,
+    desc: "Real-time posture correction and tracking.Monitors progress by tracking metrics like balance, flexibility, and pose accuracy.",
+    img: "./images/yoga-mat.jpg"
+},
+{
+    id:"def",
+    name:"Yoga Straps",
+    price: 45,
+    desc: "Yoga straps are ideal for all levels, from beginners seeking support to advanced yogis looking to deepen stretches or explore advanced poses.",
+    img: "./images/yoga-straps.jpg"
+},
+{
+    id:"ghi",
+    name:"Meditation Cushion",
+    price: 25,
+    desc: "A yoga meditation cushion is a must-have for anyone looking to enhance their meditation and seated yoga experience with comfort and mindfulness.",
+    img: "./images/medi-cushion.jpg"
+},
+{
+    id:"jkl",
+    name:"Aromatherapy Oils",
+    price: 300,
+    desc: "Promotes relaxation and stress relief.Enhances mental clarity and focus during meditation.Extracted from plants, they are pure and natural.",
+    img: "./images/aro-oils.jpg"
+},
+{
+    id:"mno",
+    name:"Yoga Bags",
+    price: 200,
+    desc: "A yoga bag is an essential accessory for anyone looking to streamline their practice while staying organized and stylish.",
+    img: "./images/yoga-bag.jpg"  
+},
+{
+    id:"pqr",
+    name:"Yoga Block",
+    price: 50,
+    desc: "An eco-friendly yoga block is a thoughtful choice for yogis who value both their practice and the planet!",
+    img: "./images/eco-block.jpg"  
+},
+{
+    id:"GHI",
+    name:"Water Bottles",
+    price: 250,
+    desc: "Modern designs in calming colors or yoga-inspired patterns, making them a perfect blend of aesthetics and utility.",
+    img: "./images/bottle.jpg"  
+},
+{
+    id:"stu",
+    name:"Instructional Books",
+    price: 45,
+    desc: "These tools bridge the gap between personal practice and professional instruction, making yoga more accessible and enjoyable for everyone.",
+    img: "./images/yoga-book.jpg"  
+},
+{
+    id:"vwx",
+    name:"Yoga Chair",
+    price: 100,
+    desc: "They are versatile, portable, and can significantly enhance the yoga experience by making postures more comfortable.",
+    img: "./images/yoga-chair.jpg"  
+},
+{
+    id:"yz",
+    name:"Fitness Band",
+    price: 500,
+    desc: "A wearable fitness band is an excellent tool for tracking your fitness goals, staying motivated, and monitoring your overall health.",
+    img: "./images/fitness-band.jpg"  
+},
+];
